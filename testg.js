60bb9ed677f7f56c616ed5354af3205e3fafc422
Coolchickenguy <chicken.science.lab@outlook.com>
2024-06-06 17:56:41 -0500

    fix dirmaping

--- testg.js
+++ testg.js
@@ -1,5 +1,6 @@
-import { getMatches } from "./lib/gitignore-executer.js";
+import { getMatches, removeMatches } from "./lib/gitignore-executer.js";
 import { dirMapFlat } from "./lib/dirMap.js";
 import fix from "./lib/es_patches.js"
 fix(import.meta);
 console.log(getMatches(dirMapFlat(__dirname),".gitignore",__dirname));
+removeMatches("./",".gitignore","./")
