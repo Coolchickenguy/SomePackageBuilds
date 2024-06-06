2d7d9f6262fcec78fc22f716bcb2212ead4513df
Coolchickenguy <chicken.science.lab@outlook.com>
2024-06-06 18:04:12 -0500

    Frogot to do stuff

--- testg.js
+++ testg.js
@@ -3,4 +3,5 @@ import { dirMapFlat } from "./lib/dirMap.js";
 import fix from "./lib/es_patches.js"
 fix(import.meta);
 console.log(getMatches(dirMapFlat(__dirname),".gitignore",__dirname));
+console.log(dirMapFlat(__dirname + "/packages"))
 removeMatches("./",".gitignore","./")
