ec36c07027deffd74ed71c4adf902447227388f5
Coolchickenguy <chicken.science.lab@outlook.com>
2024-06-07 17:06:50 -0500

    Fix directory path selector

--- cleanup.js
+++ cleanup.js
@@ -1,4 +1,4 @@
 import { removeMatches } from "./lib/gitignore-executer.js";
 import fix from "./lib/es_patches.js";
 fix(import.meta);
-removeMatches(__dirname,"./cleanup.gitignore","./");
+removeMatches(__dirname,"./cleanup.gitignore","/");
