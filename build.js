437af5a78005cd000887ad4e283ccfc83085820f
Coolchickenguy <chicken.science.lab@outlook.com>
2024-06-07 11:37:31 -0500

    Add console pipeing

--- build.js
+++ build.js
@@ -2,6 +2,7 @@ import * as fs from "fs";
 import { join } from "path";
 import { exec } from "child_process";
 import patch from "./lib/es_patches.js";
+import { stderr, stdout } from "process";
 patch(import.meta);
 var builders_dir = join(__dirname, "packages");
 var builders = fs
@@ -14,12 +15,8 @@ var output = await Promise.all(
   builders.map(
     (data) =>
       new Promise((res) => {
-        exec(
+        var task = exec(
           "node " + data.file_Name,
-          {
-            cwd: data.run_at,
-            stdio: ["inherit", "inherit", "inherit"],
-          },
           function (error, stdout, stderr) {
             if (error) {
               res({"success":false, ...arguments});
@@ -28,6 +25,8 @@ var output = await Promise.all(
             }
           }
         );
+        task.stdout.pipe(stdout);
+        task.stderr.pipe(stderr);
       })
   )
 );
