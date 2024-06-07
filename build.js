99f1f0218f0d999df03bf661ace6d91ec92bbb62
Coolchickenguy <chicken.science.lab@outlook.com>
2024-06-07 11:28:31 -0500

    Add build logging

--- build.js
+++ build.js
@@ -1,8 +1,34 @@
 import * as fs from "fs";
 import { join } from "path";
-import { execSync } from "child_process";
+import { exec } from "child_process";
 import patch from "./lib/es_patches.js";
 patch(import.meta);
-var builders_dir = join(__dirname,"packages");
-var builders = fs.readdirSync(builders_dir).filter(folder => fs.statSync(folder).isDirectory()).map(folder => {return { "file_Name" : "build.js", "run_at" : join(builders_dir,folder)}});
-builders.forEach(data => {execSync("node " + data.file_Name, { cwd: data.run_at, stdio: ["inherit", "inherit", "inherit"] });});
+var builders_dir = join(__dirname, "packages");
+var builders = fs
+  .readdirSync(builders_dir)
+  .filter((folder) => fs.statSync(folder).isDirectory())
+  .map((folder) => {
+    return { file_Name: "build.js", run_at: join(builders_dir, folder) };
+  });
+var output = await Promise.all(
+  builders.map(
+    (data) =>
+      new Promise((res) => {
+        exec(
+          "node " + data.file_Name,
+          {
+            cwd: data.run_at,
+            stdio: ["inherit", "inherit", "inherit"],
+          },
+          function (error, stdout, stderr) {
+            if (error) {
+              res({"success":false, ...arguments});
+            } else {
+              res({"success":true, stdout,stderr});
+            }
+          }
+        );
+      })
+  )
+);
+fs.writeFileSync("./packages/log.json",JSON.stringify(output));
