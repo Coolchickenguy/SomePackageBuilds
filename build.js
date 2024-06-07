7b05f4e1ab60b9b4765e0b51cab64b3f2f0a81de
Coolchickenguy <chicken.science.lab@outlook.com>
2024-06-07 11:10:40 -0500

    Allow for building with non-

--- build.js
+++ build.js
@@ -4,5 +4,5 @@ import { execSync } from "child_process";
 import patch from "./lib/es_patches.js";
 patch(import.meta);
 var builders_dir = join(__dirname,"packages");
-var builders = fs.readdirSync(builders_dir).map(folder => {return { "file_Name" : "build.js", "run_at" : join(builders_dir,folder)}});
+var builders = fs.readdirSync(builders_dir).filter(folder => fs.statSync(folder).isDirectory()).map(folder => {return { "file_Name" : "build.js", "run_at" : join(builders_dir,folder)}});
 builders.forEach(data => {execSync("node " + data.file_Name, { cwd: data.run_at, stdio: ["inherit", "inherit", "inherit"] });});
