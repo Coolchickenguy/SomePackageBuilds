97639d7298ab88c951b2640d30c80f95032b47dc
Coolchickenguy <chicken.science.lab@outlook.com>
2025-06-29 10:04:34 -0600

    Remote puppeter-releated package build and add express build/add link to index.html/ remove testg.js

--- packages/puppeteer-stream/build.js
+++
@@ -1,2 +0,0 @@
-import build from "../../lib/build-node.js";
-await build(import.meta,"https://github.com/SamuelScheit/puppeteer-stream/archive/refs/heads/main.zip");
--- testg.js
+++
@@ -1,7 +0,0 @@
-import { getMatches, removeMatches } from "./lib/gitignore-executer.js";
-import { dirMapFlat } from "./lib/dirMap.js";
-import fix from "./lib/es_patches.js"
-fix(import.meta);
-console.log(getMatches(dirMapFlat(__dirname),".gitignore",__dirname));
-console.log(dirMapFlat(__dirname + "/packages"))
-removeMatches("./",".gitignore","./")
